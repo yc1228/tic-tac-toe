0b307bf9cb00d51b0e35f560ef69d92f718bdb1b
Christine <yc1228@nyu.edu>
2017-10-10 21:08:07 -0400

    add function check_winner

--- backend/agent.py
+++ backend/agent.py
@@ -4,6 +4,12 @@ import time
 class Agent(object):
     def __init__(self, board):
         self.board = board
+        if self.board.count('X') >= self.board.count('O'):
+            self.player = 'O'
+            self.opponent = 'X'
+        else:
+            self.player = 'X'
+            self.opponent = 'O'
 
     def next_move(self):
         time.sleep(2)
@@ -11,4 +17,8 @@ class Agent(object):
         return self.board
 
     def check_winner(self):
-        pass
+        winstate = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
+        for win in winstate:
+            if self.board[win[0]] == 'X' or self.board[win[0]] == 'O':
+                if self.board[win[0]] == self.board[win[1]] and self.board[win[1]] == self.board[win[2]]:
+                    return self.board[win[0]]
